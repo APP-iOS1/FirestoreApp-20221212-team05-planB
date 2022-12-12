f90b303f86809033ac309f8b971b178ef24301b9
stealmh <kmh922@naver.com>
2022-12-12 16:20:55 +0900

    add

---
+++ 3kimWorld/_kimWorldApp.swift
@@ -0,0 +1,34 @@
+//
+//  _kimWorldApp.swift
+//  3kimWorld
+//
+//  Created by kimminho on 2022/12/12.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct _kimWorldApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
