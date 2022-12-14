91f9f272522b4ec4de6e038f7c7494ce4cb576ef
stealmh <kmh922@naver.com>
2022-12-14 16:14:53 +0900

    add paper Image

--- 3kimWorld/Views/RollingPaperView.swift
+++ 3kimWorld/Views/RollingPaperView.swift
@@ -13,26 +13,34 @@ struct RollingPaperView: View {
     var team: Team
     var member: Member
     let columns = [GridItem(.adaptive(minimum: 100))]
-    
     @State var repliesArray: [String] = ["메세지1", "메세지2", "메세지3", "메세지4", "메세지5", "메세지6", "메세지7", "메세지8"]
     
     
     var body: some View {
         
-        LazyVGrid(columns: columns, spacing: 30) {
-            ForEach(Array(rollingStore.rollingPapers.enumerated()), id: \.offset){ index, replies in
-                
-                VStack {
-                    Text(replies.message)
-                        .font(.custom("UhBee mysen", size: 25))
-                        .lineSpacing(5)
+            LazyVGrid(columns: columns, spacing: 30) {
+                ForEach(Array(rollingStore.rollingPapers.enumerated()), id: \.offset){ index, replies in
+                    
+                        VStack {
+                            Text(replies.message)
+                                .rotationEffect(.degrees(Double.random(in:0...360)))
+                                .font(.custom("UhBee mysen", size: 25))
+//                                .lineSpacing(5)
+                        }
+                    Spacer()
+
                 }
             }
-        }
-        .padding()
+
         .onAppear {
             rollingStore.fetchMessage(member: member, team: team)
         }
+        .background(
+            Image("paperImage")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        )
     }
 }
 
